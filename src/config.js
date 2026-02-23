83dd11307f868af7d8577bab1676ae977fc6cf01
html <147909829+kishcod@users.noreply.github.com>
2026-02-23 19:11:43 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:10000";
 
