6223786ec43e62da178b77e27e1564a02746e2d5
html <147909829+kishcod@users.noreply.github.com>
2026-02-23 18:57:25 +0300

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -117,11 +117,11 @@ export default function Signup() {
 
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(form),
-      });
+     const res = await fetch(`${API_URL}/api/auth/register`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(form),
+});
 
       const data = await res.json();
       if (!res.ok) {
